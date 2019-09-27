dbc105ac4f246f8d32ef1fa09f4b99a35b9f1b3f
Daniel Grychtol <grychtol.daniel@gmail.com>
2019-09-27 20:20:33 +0200

    Add routes

---
+++ client/src/components/pages/AddPost/AddPostPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddPostPage = (props: {}) => {
+  return (
+    <div>
+      <h1>add post page</h1>
+    </div>
+  );
+};
+
+export default AddPostPage;
---
+++ client/src/components/pages/NotFound/NotFoundPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFoundPage = () => {
+  return (
+    <div>
+      <h1>Page not found... :C</h1>
+    </div>
+  );
+};
+
+export default NotFoundPage;
