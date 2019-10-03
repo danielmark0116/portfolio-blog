88498d25317339d5523ba11c25b3266189ec76f7
Daniel Grychtol <grychtol.daniel@gmail.com>
2019-10-03 11:47:56 +0200

    Init task 31-1

---
+++ client/src/components/features/NavBar/NavBar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Logo from '../../common/Logo/Logo';
+import MainMenu from '../../layout/MainMenu/MainMenu';
+
+import style from './NavBar.module.scss';
+
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      links: [
+        { path: '/', title: 'Home' },
+        { path: '/posts/new', title: 'Add post' },
+        { path: '/posts', title: 'Posts' },
+        { path: '/contact', title: 'Contact' }
+      ]
+    };
+  }
+
+  render() {
+    const { links } = this.state;
+
+    return (
+      <nav className={style.navbar}>
+        <Logo links="" location=""></Logo>
+        <MainMenu links={links}></MainMenu>
+      </nav>
+    );
+  }
+}
+
+export default NavBar;
--- client/src/components/layout/MainMenu/MainMenu.js
+++ client/src/components/layout/MainMenu/MainMenu.js
@@ -6,19 +6,26 @@ import './MainMenu.scss';
 
 const MainMenu = ({ links, location }) => (
   <ul className="main-menu">
-    { links.map((link, index) =>
+    {links.map((link, index) => (
       <li key={index}>
-        <Link className={(location.pathname === link.path && 'active') || ''} to={link.path}>{link.title}</Link>
+        <Link
+          className={(location.pathname === link.path && 'active') || ''}
+          to={link.path}
+        >
+          {link.title}
+        </Link>
       </li>
-    )}
+    ))}
   </ul>
 );
 
 MainMenu.propTypes = {
-  links: PropTypes.arrayOf(PropTypes.shape({
-    path: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-  })),
+  links: PropTypes.arrayOf(
+    PropTypes.shape({
+      path: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired
+    })
+  )
 };
 
-export default withRouter(props => <MainMenu {...props}/>);
+export default withRouter(props => <MainMenu {...props} />);
