fbd9edef738086aa0b876c96f42c5670f63f2c2d
Daniel Grychtol <grychtol.daniel@gmail.com>
2019-10-08 10:39:57 +0200

    Fix

--- server/server.js
+++ server/server.js
@@ -13,17 +13,15 @@ dbConnection.connectToDb();
 
 app.use(helmet());
 app.use(cors());
-app.use(express.static(path.join(__dirname + '/../client/build')));
+app.use(express.static(path.join(__dirname + '../client/build')));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use('/api', postRoutes);
 
-if (process.env.MODE === 'production') {
-  app.use('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '../client/build/index.html'));
-  });
-}
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '../client/build/index.html'));
+});
 
 console.log(process.env.MODE);
 
