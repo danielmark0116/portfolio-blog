44683369d71308b5c2cc6b69b6aac82976d770f9
Daniel Grychtol <grychtol.daniel@gmail.com>
2019-10-08 09:40:10 +0200

    Fix port

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  PORT: 8000,
+  PORT: process.env.PORT || 8000,
   DB:
     'mongodb+srv://blog-mern-user-admin:admin@cluster0-8awro.mongodb.net/retryWrites=true&w=majority'
 };
