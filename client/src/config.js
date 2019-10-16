2e00b7ff510ee285a3b4354934e487cc1aa581be
Daniel Grychtol <grychtol.daniel@gmail.com>
2019-10-17 00:34:49 +0200

    Client config fix

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,6 @@
 export const BASE_URL = process.env.BASE_URL || 'http://www.localhost:3000';
 export const FACEBOOK_APP_ID = '561114797761802';
 export const URL_PREFIX =
-  process.env.MODE === 'production' ? '/blog' : 'http://localhost:8001';
+  process.env.REACT_APP_MODE === 'production'
+    ? '/blog'
+    : 'http://localhost:8001';
