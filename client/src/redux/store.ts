f6fe9b6df40eb31bb2064043779e7824deadc073
Daniel Grychtol <grychtol.daniel@gmail.com>
2019-10-16 16:28:07 +0200

    Store fix

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -8,19 +8,15 @@ const rootReducer = combineReducers({
   postsReducer
 });
 
-const middlewareSetup = () => {
-  if (process.env.REACT_APP_MODE === 'production') {
-    return compose(
-      applyMiddleware(thunk as ThunkMiddleware<AppState, ActionTypes>)
-    );
-  }
-  return compose(
-    applyMiddleware(thunk as ThunkMiddleware<AppState, ActionTypes>),
-    (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
-      (window as any).__REDUX_DEVTOOLS_EXTENSION__()
-  );
-};
+const middleware =
+  process.env.REACT_APP_MODE === 'development'
+    ? compose(
+        applyMiddleware(thunk as ThunkMiddleware<AppState, ActionTypes>),
+        (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
+          (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+      )
+    : compose(applyMiddleware(thunk as ThunkMiddleware<AppState, ActionTypes>));
 
-export const store = createStore(rootReducer, middlewareSetup());
+export const store = createStore(rootReducer, middleware);
 
 export type AppState = ReturnType<typeof rootReducer>;
